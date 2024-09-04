48bdd7ed3a488c74cc04d794c5233cd7140fdded
Jean <jeanmatheus0612@gmail.com>
2024-09-04 11:26:13 -0300

    Criando o NavMenu e aplicando ele

---
+++ src/styled.js
@@ -0,0 +1,24 @@
+import  styled  from "styled-components";
+
+export const NavMenu = styled.nav`
+    width: 100%;
+    min-height: 8vh;
+    padding: 20px;
+    background-color: tomato;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    a{
+        text-decoration: none;
+        color: #fff;
+        font-size: 1.3em;
+    }
+
+    span{
+        color: #fff;
+        font-size: 1.5em;
+    }
+
+
+`
